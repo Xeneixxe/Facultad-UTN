f824589c8d383229843d2f764ff2bb912e96c68f
Franco Morales <francomorale36@gmail.com>
2024-09-24 17:11:47 -0300

    Agrego Ejercicios

---
+++ Python/Segundo Semestre/Ejercicio06.py
@@ -0,0 +1,8 @@
+#Comenzamos con Funciones
+#mi_funcion()# No se puede llamar antes de definir una función
+#Definimos una función
+def mi_funcion():
+    print('Saludos a todos los campeones del mundo ')
+
+mi_funcion() #Estamos llamando a la función
+mi_funcion() #Se puede llamar a una funcion N cantidad de veces
