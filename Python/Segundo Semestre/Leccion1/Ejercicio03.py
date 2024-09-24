628b3e481a135e1bcaa5dc4f3714b21d987e0d27
Franco Morales <francomorale36@gmail.com>
2024-09-24 16:17:50 -0300

    Se agregan Ejercicios de Python

--- Python/Segundo Semestre/Leccion1/Ejercicio03.py
+++ Python/Segundo Semestre/Leccion1/Ejercicio03.py
@@ -1,16 +1,16 @@
-#Ejercicio 3: Insertar elementos y ordenarlos
-#Pedir numero y meterlos en una lista, cuando el usuario
-#Introduzca un numero 0, nuestro programa dejaria de insertar .
-#Por ultimo, mostrar los numero ordenados de menor a mayor
+# Ejercicio 3: Insertar elementos y ordenarlos
+# Pedir numero y meterlos en una lista, cuando el usuario
+# Introduzca un numero 0, nuestro programa dejaria de insertar .
+# Por ultimo, mostrar los numero ordenados de menor a mayor
 
 lista = []
 salir = False
 while not salir:
-    numero = int(input('Digite un número: '))
+    numero = int(input("Digite un número: "))
     if numero == 0:
         salir = True
     else:
         lista.append(numero)
-    
-lista.sort()#La lista esta ordenada con esta función
-print(f'\nLista ordenada: \n{lista}')
+
+lista.sort()  # La lista esta ordenada con esta función
+print(f"\nLista ordenada: \n{lista}")
