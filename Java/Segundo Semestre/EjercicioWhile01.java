0e42eb2e6d0cf7744748d2ae3620d0fbaa04a5fb
Franco Morales <francomorale36@gmail.com>
2024-09-25 10:48:38 -0300

    Clase 1 Java

--- Java/Segundo Semestre/EjercicioWhile01.java
+++ Java/Segundo Semestre/EjercicioWhile01.java
@@ -12,17 +12,19 @@ public class EjercicioWhile01 {
             contador++;
 
         } while (contador < 7);
-
+        //Uso de las palabras breack y continue junto a las etiquetas(labels)
+        
         for (var contando = 0; contador < 7; contador++) {
             if (contando % 2 == 0) {
                 System.out.println("contando =" + contando);
                 break;
             }
         }
+        inicio:
         for (var contando = 0; contador < 7; contador++) {
             if (contando % 2 == 1) {
-                continue; // Vamos a la siguiente iteración
-            }
+                continue inicio; // Vamos a la siguiente iteración
+            }//Etiquetas Labels se utilizan en ciclos anidados 
             System.out.println("contando =" + contando);
         }
     }
